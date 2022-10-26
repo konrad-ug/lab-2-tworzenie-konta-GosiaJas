458eefd6616cdba38af783c2d2fd9eaf8d494335
Gosia Jasińska <gosiajasinska@MacBook-Air-Gosia.local>
2022-10-26 12:56:19 +0200

    testowy push

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,11 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, kod):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+        self.pesel = pesel
+        if(kod == None):
+            self.kod = None
+        else:
+            if(kod[0] == "P" and kod[1] == "R" and kod[2] == "O" and kod[3] == "M" and kod[4] == "_" and len(kod) == 8):
+                self.saldo = 50
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -4,10 +4,12 @@ from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
 
-    def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+    def f3(self):
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "12345678910", "PROM_23")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, "12345678910", "Pesel nie został zapisany") #f2
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Niepoprawny pesel") #f3
 
-    #tutaj proszę dodawać nowe testy
+
